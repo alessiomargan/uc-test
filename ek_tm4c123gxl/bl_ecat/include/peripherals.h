caf9255888b044e6522a681092f637722441894c
Alessio Margan <alessio.margan@iit.it>
2021-05-06 12:03:09 +0200

    move tm4c bootloader

--- ek_tm4c123gxl/bl_ecat/include/peripherals.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PERIPHERALS_H__
-#define __PERIPHERALS_H__
-
-void Configure_UART(void);
-void Configure_EcatPDI(void);
-void Configure_GPIO(void);
-
-#endif
--- ek_tm4c123gxl/soes_test/build.sh
+++
@@ -1,1 +0,0 @@
-make clean ; make ; cp gcc/ecat.bin /usr/local/src/catkin_ws/src/soem/build/
