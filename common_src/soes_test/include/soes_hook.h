ad05af790daf36b86cfadf2614c967f6a34d731d
alessio marganiit.it <alessio.margan@iit.it>
2025-01-20 09:56:11 +0100

    use cch.h for PACKED

--- common_src/soes_test/include/soes_hook.h
+++ common_src/soes_test/include/soes_hook.h
@@ -1,18 +1,12 @@
 #ifndef __SOES_HOOK_H__
 #define __SOES_HOOK_H__
 
-#ifdef IAR
-	#define IAR_PACKED 	__packed
-	#define CCS_PACKED
-#else
-	#define IAR_PACKED
-	#define CCS_PACKED	__attribute__((__packed__))
-#endif
+#include <cc.h>
 
 typedef struct {
-	uint8_t		warn_read_flash:1;
-	uint8_t		warn:7;
-	uint8_t		err:8;
+	uint16_t		warn_read_flash:1;
+	uint16_t		warn:7;
+	uint16_t		err:8;
 } bit_fault_t;
 
 typedef union{
@@ -20,7 +14,7 @@ typedef union{
     bit_fault_t bit;
 } fault_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
 	float       pos_ref;  				// motor position reference
 	float    	vel_ref;  				// motor velocity reference
 	float     	tor_ref;  				// torque reference
@@ -30,9 +24,9 @@ typedef IAR_PACKED struct {
 	uint16_t    op_idx_aux;    			// op [get/set] , idx
 	float       aux;        			// set value
 
-} CCS_PACKED rx_pdo_t;
+} CC_PACKED rx_pdo_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
     float       link_pos;           	// link position (rad)
     float       motor_pos;           	// motor position (rad)
     float      	link_vel;             	// link velocity  (mrad/sec)
@@ -44,9 +38,9 @@ typedef IAR_PACKED struct {
     uint16_t    op_idx_ack;         	// op [ack/nack] , idx
     float       aux;                	// get value or nack erro code
 
-} CCS_PACKED tx_pdo_t;
+} CC_PACKED tx_pdo_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
 
     // 0x8000 flash param
 	uint32_t	_signature_;
@@ -55,9 +49,9 @@ typedef IAR_PACKED struct {
 	uint8_t     blob[16];
 	uint32_t    analog_sample_freq;
 
-} CCS_PACKED flash_sdo_t;
+} CC_PACKED flash_sdo_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
 
     // 0x8001 ram param
     char 		fw_ver[8];
@@ -67,16 +61,16 @@ typedef IAR_PACKED struct {
 	uint16_t 	flash_params_cmd_ack;
 	uint16_t 	ack_board_faults;
 
-} CCS_PACKED ram_sdo_t;
+} CC_PACKED ram_sdo_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
 
     flash_sdo_t	flash;
     ram_sdo_t	ram;
 
-} CCS_PACKED sdo_t;
+} CC_PACKED sdo_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
     // 0x8002 aux param
 	float rtt;
     float volt_ref;
@@ -84,9 +78,9 @@ typedef IAR_PACKED struct {
     float vout;
     float pos_ref_fb;
 
-} CCS_PACKED aux_pdo_tx_t;
+} CC_PACKED aux_pdo_tx_t;
 
-typedef IAR_PACKED struct {
+typedef struct {
     // 0x8003 aux param
 	float ts;
     float pwm_duty;
@@ -102,7 +96,7 @@ typedef IAR_PACKED struct {
     float		var_float;
     double		var_double;
 
-} CCS_PACKED aux_pdo_rx_t;
+} CC_PACKED aux_pdo_rx_t;
 
 void ecat_process_pdo(void);
 void bootstrap_foe_init(void);
