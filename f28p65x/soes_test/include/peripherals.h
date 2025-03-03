7a34134333c2e9712f1dee7859705c4cb11c2b4e
alessio marganiit.it <alessio.margan@iit.it>
2025-03-03 08:34:59 +0100

    [f28p65x] add  serial printf

---
+++ f28p65x/soes_test/include/peripherals.h
@@ -0,0 +1,16 @@
+/*
+ * peripherals.h
+ *
+ *  Created on: Mar 3, 2025
+ *      Author: amargan
+ */
+
+#ifndef SOES_TEST_INCLUDE_PERIPHERALS_H_
+#define SOES_TEST_INCLUDE_PERIPHERALS_H_
+
+void gpio_conf(void);
+
+void scia_init(void);
+
+
+#endif /* SOES_TEST_INCLUDE_PERIPHERALS_H_ */
